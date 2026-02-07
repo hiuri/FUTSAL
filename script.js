58ebb2e5ccdcedb1ea3e9ffd385c80ffbcbe64fc
Hiuri Carriço Liberato <hiuri3112@hotmail.com>
2026-02-07 01:21:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ fetch('dados.json')
   .then(res => res.json())
   .then(data => {
 
+    // =========================
+    // INICIALIZA TABELA COM TODOS OS TIMES
+    // =========================
     const tabela = {};
 
+    Object.values(data.grupos).flat().forEach(time => {
+      tabela[time] = {
+        pts: 0,
+        j: 0,
+        v: 0,
+        e: 0,
+        d: 0,
+        gp: 0,
+        gc: 0
+      };
+    });
+
     // =========================
-    // PROCESSA RESULTADOS (GRUPOS)
+    // PROCESSA RESULTADOS (FASE DE GRUPOS)
     // =========================
     data.tabela_resultados.forEach(jogo => {
 
       if (jogo.fase !== "Grupos") return;
       if (jogo.gols_mandante === null || jogo.gols_visitante === null) return;
-
-      // Garante que os times existem
-      if (!tabela[jogo.mandante]) {
-        tabela[jogo.mandante] = { pts:0, j:0, v:0, e:0, d:0, gp:0, gc:0 };
-      }
-      if (!tabela[jogo.visitante]) {
-        tabela[jogo.visitante] = { pts:0, j:0, v:0, e:0, d:0, gp:0, gc:0 };
-      }
 
       const mandante = tabela[jogo.mandante];
       const visitante = tabela[jogo.visitante];
@@ -57,12 +64,12 @@ fetch('dados.json')
     corpo.innerHTML = "";
 
     Object.entries(tabela)
-      .sort((a,b)=> 
+      .sort((a, b) =>
         b[1].pts - a[1].pts ||
         (b[1].gp - b[1].gc) - (a[1].gp - a[1].gc) ||
         b[1].gp - a[1].gp
       )
-      .forEach(([time,t])=>{
+      .forEach(([time, t]) => {
         corpo.innerHTML += `
           <tr>
             <td>${time}</td>
@@ -84,17 +91,18 @@ fetch('dados.json')
     art.innerHTML = "";
 
     data.tabela_artilharia
-      .sort((a,b)=>b.gols - a.gols)
-      .forEach(j=>{
+      .sort((a, b) => b.gols - a.gols)
+      .forEach(j => {
         art.innerHTML += `
           <tr>
             <td>${j.jogador}</td>
+            <td>${j.time}</td>
             <td>${j.gols}</td>
           </tr>`;
       });
 
     // =========================
-    // TABELA DE JOGOS + MATA-MATA
+    // TABELA DE JOGOS (GRUPOS) + MATA-MATA
     // =========================
     const jogos = document.querySelector('#jogos tbody');
     const mata = document.querySelector('#mataMata tbody');
@@ -102,7 +110,8 @@ fetch('dados.json')
     jogos.innerHTML = "";
     mata.innerHTML = "";
 
-    data.tabela_resultados.forEach(j=>{
+    data.tabela_resultados.forEach(j => {
+
       const placar = (j.gols_mandante !== null)
         ? `${j.gols_mandante} x ${j.gols_visitante}`
         : "x";
@@ -115,7 +124,7 @@ fetch('dados.json')
             <td>${j.mandante}</td>
             <td>${placar}</td>
             <td>${j.visitante}</td>
-            <td>${j.fase}</td>
+            <td>${j.grupo}</td>
           </tr>`;
       } else {
         mata.innerHTML += `
